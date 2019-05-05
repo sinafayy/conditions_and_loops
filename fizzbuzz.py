cdaf85420a66d81dbe935eb3c4459142213af999
Ana <ana.mihojevic@gmail.com>
2019-05-05 18:40:17 +0200

    forth commit

---
+++ fizzbuzz.py
@@ -0,0 +1,14 @@
+print("Welcom to Ane's fizzbuzz game! Let thegames begin!")
+
+end = input("Please enter a number between 1 and 100: ")
+end = int(end)
+
+for num in range(1, end+1):
+    if num % 3 == 0 and num % 5 == 0:
+        print("fizzbuzz")
+    elif num % 3 == 0:
+        print("fizz")
+    elif num % 5 == 0:
+        print("buzz")
+    else:
+        print(num)
---
+++ join_strings.py
@@ -0,0 +1,4 @@
+str_one = "Happy"
+str_two = "Day"
+
+print("{0} {1}".format(str_one, str_two))
